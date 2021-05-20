11bdd35e8e7ff2e8d3f6e8d0490d864c862c7539
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-21 07:56:45 +0900

    なんとなく

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const discord = require('discord.js');
-const levens = require('levenshtein');
+const leven = require('levenshtein');
 const fs = require('fs');
 const path = require('path');
 const commandArgs = require('./commands.json');
@@ -12,6 +12,7 @@ global.client = new Client({
     intent: Intents.NON_PRIVILEGED
   }
 });
+global.queue = new Map();
 
 client.on('ready', () => {
   let list = fs.readdirSync(path.join(__dirname, 'commands'))
@@ -23,7 +24,33 @@ client.on('ready', () => {
   };
 });
 
-client.on('message', message => {
+client.on('message', async message => {
   if(message.author.bot || message.system || (!message.content.startsWith(prefix) || (!message.mentions.users.has(client.user) && !message.content.match(new RegExp(`<@!?${client.user.id}>`)))))
     return;
+  const args = SpaceSplit(message.content.slice(prefix.length));
+  const command = args.shift();
+  const commandList = Object.keys(commands);
+  if (commandList.includes(command)) {
+    let cursor = commandArgs[command];
+    if (cursor.args.some(x => x.length === args.length)) {
+      let result;
+      try {
+        result = await commands[command](message, args, client);
+      } catch {
+        result = ':x: Something went wrong.....';
+      };
+      if(result) return message.channel.send(result);
+    } else {
+      return message.reply(':x: 引数が間違っています。引数は`' + prefix + 'help ' + command + '`で確認してください。');
+    };
+  } else {
+    let dym = commandList
+      .reduce((acc, cur) => {
+        let { distance } = new leven(cur, input);
+        return distance < acc[0] ? [distance, cur] : acc;
+      }, [3, ""])[1];
+     return dym ?
+     message.reply(':thinking: コマンドを間違えているようです。\nもしかして: ' dym) :
+     void 0;
+  };
 });
