05ef83f5ee70074ecb518a9ed58b952c84259cbb
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-21 07:03:15 +0900

    うーんこの

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const discord = require('discord.js\')
+const discord = require('discord.js');
+const levens = require('levenshtein');
+const fs = require('fs');
+const path = require('path');
+const commandArgs = require('./commands.json');
+const commands = {};
+const prefix = '%';
+const { Client, MessageEmbed, Intents, MessageAttachment } = discord;
+global.client = new Client({
+  intent: Intents.NON_PRIVILEGED,
+  ws: {
+    intent: Intents.NON_PRIVILEGED
+  }
+});
+
+client.on('ready', () => {
+  let list = fs.readdirSync(path.join(__dirname, 'commands'))
+    .map(x => x.replace(/\.js$/,''));
+  for(command of list){
+    let run = require(path.join(__dirname, 'commands', command));
+    commands[command] = run;
+    console.log('loaded \'' + command + '\'');
+  };
+});
+
+client.on('message', message => {
+  if(message.author.bot || message.system || (message.content.startsWith(prefix) || (message.mentions.users.has(client.user) && message.content.match(new RegExp(`<@!?${client.user.id}>`)))))
+    return;
+});
