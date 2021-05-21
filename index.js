402db1844331481c2bb96f03b95ed691b9fb9489
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-21 12:44:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ global.queue = new Map();
 client.on('ready', () => {
   let list = fs.readdirSync(path.join(__dirname, 'commands'))
     .map(x => x.replace(/\.js$/,''));
-  for(command of list){
+  for(let command of list){
     let run = require(path.join(__dirname, 'commands', command));
     commands[command] = run;
     console.log('loaded \'' + command + '\'');
