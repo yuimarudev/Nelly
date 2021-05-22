15b6b72bc2fbfd797adcda4ef74a5cde8155d1b1
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-23 01:26:08 +0900

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -1,5 +1,8 @@
-module.exports = message => {
+module.exports = async message => {
     const q = queues.get(message.guild.id);
+    if (!q) {
+      return void await message.reply(":x: The queue is void.");
+    }
     const enabled = (q.loop = !q.loop);
     await message.reply(":repeat: Queue Loop " + (enabled ? "ON" : "OFF"));
 }
