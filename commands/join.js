240e199bca3de6ee5a1ae8da3fbc0286f88979e9
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-22 10:51:49 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,4 +1,4 @@
-module.exports = async (message, _, _) => {
+module.exports = async (message, _, __) => {
     message.member.voice?.channel.join()
       .then(conn => queue.set(message.guild.id, { connection: conn, textChannel: message.channel, voiceChannel: conn.channel }))
       .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\nエラー内容: ${err}`))
