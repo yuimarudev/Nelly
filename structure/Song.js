7ab8c6186a8d5e06e06e14a84104aea3dde93439
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-23 00:06:57 +0900

    Update Song.js

--- structure/Song.js
+++ structure/Song.js
@@ -2,7 +2,7 @@ module.exports = class {
   constructor(data) {
     let details = data.videoDetails;
     this.title = details.title;
-    this.url = detail.video_url;
+    this.url = details.video_url;
     this.thumbnail = details.thumbnails[details.thumbnails.length - 1];
     this.duration = details.lengthSeconds;
     this.author = details.author;
