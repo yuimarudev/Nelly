de90e30af3214e1e7088176641ee609a945ff723
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-22 10:32:52 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,4 +1,4 @@
-async module.exports = (message, _, _) => {
+module.exports = async (message, _, _) => {
     message.member.voice.channel.join()
       .then(conn => queue.set(message.guild.id, conn))
       .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\nエラー内容: ${err}`))
