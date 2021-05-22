2d82583ca404444883badbb0aa689a9cf8ad02c1
#⃣ <>
2021-05-22 01:17:25 +0000

    typo fixed (lie)

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,5 @@
-async module.exports = (client, _, message) => {
+async module.exports = (message, _, _) => {
     message.member.voice.channel.join()
       .then(conn => queue.set(message.guild.id, conn))
-      .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\n内容: ${err}`))
+      .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\nエラー内容: ${err}`))
 }
