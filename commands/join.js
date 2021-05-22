369345b0996e3b6f0fcf9ceaaef9b3f7cfcf2d5e
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-22 19:54:26 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,8 +1,8 @@
-const Song = require('../structure/Song.js');
+const Queue = require('../structure/Queue.js');
 
 module.exports = async message => {
     message.member.voice?.channel.join()
-      .then(conn => queues.set(message.guild.id, new Song(message, conn)))
+      .then(conn => queues.set(message.guild.id, new Queue(message, conn)))
       .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\nエラー内容: ${err}`))
     ? void 0
     : message.channel.send('ボイスチャンネルに参加してください(  ˙༥˙  )')
