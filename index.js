d6c9620d38831dfb47337fa76075cb0479a85d70
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-22 11:56:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -64,9 +64,10 @@ client.on('message', async message => {
   const commandDict = commandArgs.commands;
   const aliasList = Object.keys(commandArgs.aliases);
   const curs = (command in commandDict ? command : false) || (aliasList[command] in commandDict ? aliasList[command] : false);
+  await message.reply(command);
   if (curs) {
     let cursor = commandArgs.commands[curs];
-    if (cursor.args.some(x => x.length === args.length)) {
+    if (true || cursor.args.some(x => x.length === args.length)) {
       let result;
       try {
         result = await commands[curs](message, args, client);
