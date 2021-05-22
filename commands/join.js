fbc7ad1980701099750a59cd137257bc2de2d016
#⃣ <>
2021-05-22 01:46:14 +0000

    a

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,7 @@
 module.exports = async (message, _, _) => {
-    message.member.voice.channel.join()
+    message.member.voice?.channel.join()
       .then(conn => queue.set(message.guild.id, { connection: conn, textChannel: message.channel, voiceChannel: conn.channel }))
       .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\nエラー内容: ${err}`))
+    ? void 0
+    : message.channel.send('ボイスチャンネルに参加してください><')
 }
