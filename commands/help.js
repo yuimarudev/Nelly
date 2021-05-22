f8f8a8193ce7dab28fefa3a188764059451dea70
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-22 14:13:03 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,28 +1,27 @@
 module.exports = (message, args, client) => {
-	const dict = require('../commands.json');
-	if (args?.length) {
-		const info = dict[args[0]];
-		info
-			? message.channel.send(
-					MessageEmbed({
-						title: args[0],
-						description: info['description'],
-						fields: [
-							{
-								name: '詳細',
-								value: info['details']
-							},
-							{
-								name: '引数',
-								value: `%${args[0]} ${info["args"].map(
-									content => '<' + content.join('│') + '>'
-								).join(' ')}`
-							}
-						]
-					})
-			  )
-			: message.channel.send('無効なコマンドです');
-	} else {
-	    message.channel.send('・'+Object.values(dict["aliases"]).join('\n・'))
-	}
+  const dict = require('../commands.json');
+  if (args.length) {
+    const info = dict.commands[args[0]];
+    if (info) {
+      message.channel.send({
+        embed: {
+          title: args[0],
+          description: info.description,
+          fields: [{
+            name: '詳細',
+            value: dict.details
+          }, {
+            name: '引数',
+            value: `%${args[0]} ${info.args.map(
+              content => '<' + content.join('│') + '>'
+            ).join(' ')}`
+          }]
+        }
+      });
+    } else {
+      message.channel.send('無効なコマンドです');
+    }
+  } else {
+    message.channel.send(Object.keys(dict.commands).map(v => '・' + v).join('\n'))
+  }
 };
