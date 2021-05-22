7585d5f69ac6a79bb068db87af1f5f1c0b282e86
IdioticDog <83163937+IdioticDog@users.noreply.github.com>
2021-05-22 15:24:04 +0900

    Corresponded lower version (than Node v14)
    Removed optional chain

--- commands/join.js
+++ commands/join.js
@@ -1,7 +1,7 @@
 module.exports = async message => {
-    message.member.voice?.channel.join()
+    message.member.voice.channel
+    ? message.member.voice.channel.join()
       .then(conn => queue.set(message.guild.id, { connection: conn, textChannel: message.channel, voiceChannel: conn.channel }))
       .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\nエラー内容: ${err}`))
-    ? void 0
     : message.channel.send('ボイスチャンネルに参加してください(  ˙༥˙  )')
 }
