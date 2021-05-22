9e4bedda9b00d88c9683ef4338b2658a0d0cc85d
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-22 22:51:48 +0900

    Update Queue.js

--- structure/Queue.js
+++ structure/Queue.js
@@ -6,12 +6,30 @@ module.exports = class {
     this.voiceChannel = message.member.voice.channel;
     this.connection = connection;
     this.songs = [];
+    this.loop = false;
     this.volume = 5;
     this.playing = false;
   }
   async addAudio(url) {
     const info = await ytdl.getInfo(query);
     this.songs.push(new Song(info));
+    if (!this.playing && this.connection) {
+      play(this);
+    }
     return info;
   }
+}
+
+function play(queue) {
+  if (!this.songs.length) return;
+  const song = queue.songs.shift();
+  const stream = ytdl.downloadFromInfo(song._info);
+  queue.connection.play(stream)
+  .on('finish', () => {
+    if (queue.loop) queue.songs.push(song);
+    play(queue);
+  })
+  .on('error', err => {
+    console.error(err);
+  });
 }
