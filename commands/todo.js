c1a9481e6733e85e000294f17b1acca4a1f74c99
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-22 09:26:41 +0900

    Create todo.js

--- commands/todo.js
+++ commands/todo.js
@@ -1,3 +1,31 @@
 module.exports = async (message, args, client) => {
-  return void await message.channel.send(`:slight_frown: This is not suppoted yet.`);
-};
+  switch (args[0]) {
+    case "show":
+      showList(message, args, client);
+      break;
+    case "add":
+      addToList(message, args, client);
+    case "del":
+      deleteElement(message, args, client);
+    default:
+      return void await message.channel.send(`:x: 第一引数の値が無効です。`);
+  }
+}
+
+function showList(m) {
+  return void await m.channel.send(
+    `:slight_frown: This is not supported yet.`
+  );
+}
+
+function addToList(m) {
+  return void await m.channel.send(
+    `:slight_frown: This is not supported yet.`
+  );
+}
+
+function deleteElement(m) {
+  return void await m.channel.send(
+    `:slight_frown: This is not supported yet.`
+  );
+}
