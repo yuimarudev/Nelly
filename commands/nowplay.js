eff287ed84418656213f99e82c9975006260c62f
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-23 11:18:24 +0900

    Update nowplay.js

--- commands/nowplay.js
+++ commands/nowplay.js
@@ -7,7 +7,7 @@ module.exports = async(message) => {
   .setAuthor(song.member.tag, song.member.user.avatarURL({size:512, format:'png'}) )
   .setURL(song.url)
   .setThumbnail(song.thumbnail)
-  .setFooter(song.author.thumbnails.first().url, song.author.name)
+  .setFooter(song.author.thumbnails[0].url, song.author.name)
   .setDescription(serverQueue.dispatcher.streamTime + '/' + song.duration)
   .setColor(0x00ff00);
   message.channel.send([embed]);
