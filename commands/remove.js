8cce477e55314d8cd2ee1be62bbbad41e86bcd6d
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-23 14:56:28 +0900

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,8 +1,10 @@
 module.exports = async (message, args) => {
     let { textChannel, songs } = queues.get(message.guild.id);
+    if (songs.length < args.length)
+        return void await message.reply(":x: 引数の数が多すぎます。");
     const msg = await message.channel.send('ちょっと待ってね！(   ◜ω◝ )');
     const eliminated = [];
-    args.forEach(i => {
+    [...Set(args)].forEach(i => {
         eliminated.push(songs[i]);
         songs[i] = void 0;
     });
