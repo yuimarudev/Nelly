8cda356b906fbe9ca167efdf25b417082df1dbab
IdioticDog <83163937+IdioticDog@users.noreply.github.com>
2021-05-23 14:01:22 +0900

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,8 +1,9 @@
 module.exports = async (message, args) => {
-    const { textChannel, songs } = queues.get(message.guild.id);
+    let { textChannel, songs } = queues.get(message.guild.id);
     const msg = await textChannel.send('ちょっと待ってね！(   ◜ω◝ )');
     args.forEach((i, index) => {
-      songs.splice(i - 1, 1);
+      songs[i] = undefined;
       msg.edit(`${index + 1}曲削除したよ！( ◜௰◝  ）`);
     });
+    songs = songs.filter(obj => obj);
 }
