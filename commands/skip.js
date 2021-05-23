0576dc21d79aeb752e84e86475cbfba7f084ea78
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-23 10:36:53 +0900

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,2 +1,7 @@
-module.exports = message => {
+module.exports = await message => {
+  const queue = queues.get(message.guild.id);
+  try {
+    queue.dispatcher.destroy();
+    await message.reply(":fast_forward: Skipped!");
+  } catch { }
 };
