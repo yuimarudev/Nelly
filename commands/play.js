4b32248d0f4ca962f1fc96d95ec2db2ee29c1259
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-23 18:08:17 +0900

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -57,9 +57,15 @@ module.exports = async(message, args, client) => {
            let playlist = await ytpl(args[0]), addCount = 0;
            for (let v of playlist.items) {
               await serverQueue.addMusic(v.url, message).then(() => addCount++, _=>0);
+              await delay(1500);
            }
            if (1 < addCount) await message.channel.send(`:white_check_mark: Added ${addCount} songs!`);
            else await message.channel.send(`:white_check_mark: Added ${addCount ? "1" : "no"} song!`);
         }
     }
+}
+
+function delay(ms) {
+  if (typeof ms !== "number" || ms < 1) ms = 1000;
+  return new Promise(r => setTimeout(r, ms));
 }
