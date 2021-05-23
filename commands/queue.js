ccc5dbaf1805e6ba626c333ed0f8cb8d785f3046
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-23 09:29:01 +0900

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -2,12 +2,16 @@ module.exports = async message => {
   const serverQueue = queues.get(message.guild.id);
   if (!serverQueue) {
     return void await message.reply(
-      ":x: The queue is empty."
+      ":x: There is no queue."
     );
   }
-  const songs = [...serverQueue.values()].map(s => `[${s.title}](${s.url})`);
+  const songs = serverQueue.songs.length ?
+    serverQueue.songs
+    .map(s => `[${s.title}](${s.url})`)
+    .join('\n'):
+    "The queue is empty";
   await message.channel.send({ embed: {
     title: "Queue",
-    description: songs.join('\n')
+    description: songs
   }});
 };
