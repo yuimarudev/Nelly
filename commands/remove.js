b60ce326e16bb8c053652d76bac84d41d7c79e2a
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-23 20:24:17 +0900

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -5,6 +5,7 @@ module.exports = async (message, args) => {
     const msg = await message.channel.send('ちょっと待ってね！(   ◜ω◝ )');
     const eliminated = [];
     [...new Set(args)].forEach(i => {
+        i = +i-1;
         eliminated.push(songs[i]);
         songs[i] = void 0;
     });
