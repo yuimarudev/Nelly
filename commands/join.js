e0f20e8aa9a675dc29e47696bf31db5a5338b1fc
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-25 07:56:04 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -3,7 +3,7 @@ const Queue = require('../structure/Queue.js');
 module.exports = async message => {
     message.member.voice?.channel.join()
       .then(conn => queues.set(message.guild.id, new Queue(message, conn)))
-      .catch(err => message.channel.send(`おっと、問題が発生したみたいですね\nエラー内容: ${err}`))
+      .catch(err => message.channel.send(`${Messages.AccidentMessage}\nエラー内容: ${err}`))
     ? void 0
-    : message.channel.send('ボイスチャンネルに参加してください(  ˙༥˙  )')
+    : message.channel.send(Messages.PleaseJoinVoiceChannelMessage)
 }
