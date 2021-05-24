ee3f22f59516815238c668d233a851ac0ac1daf4
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-24 14:50:34 +0900

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,5 +1,15 @@
 module.exports = async message => {
   const queue = queues.get(message.guild.id);
+  if (!queue)
+    return void await message.reply(":x: There is no queue.");
+  const now = queue.playingSong;
+  if (!now)
+    return void await message.reply(":x: No music is playing.");
+  const next = queue.songs[0];
+  if (now.loop) {
+    now.loop = false;
+    if (next) next.loop = true;
+  }
   try {
     queue.dispatcher.emit('finish');
     await message.reply(":fast_forward: Skipped!");
