814d2778fb0c9c693ec276f0b0e39f524e274736
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-25 07:58:02 +0900

    Update loopqueue.js

--- commands/loopqueue.js
+++ commands/loopqueue.js
@@ -1,8 +1,8 @@
 module.exports = async message => {
     const q = queues.get(message.guild.id);
     if (!q) {
-      return void await message.reply(":x: The queue is void.");
+      return void await message.reply(Messages.NoQueue);
     }
     const enabled = (q.loop = !q.loop);
-    await message.reply(":repeat: Queue Loop " + (enabled ? "ON" : "OFF"));
+    await message.reply(Messages.QueueLoop + (enabled ? "ON" : "OFF"));
 }
