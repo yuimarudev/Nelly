2a28b5e205497c97577b44d0aa2d0d5ca8dc9758
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-24 18:10:27 +0900

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -6,29 +6,29 @@ const regex = /https?:\/\/youtu(?:be\.com|\.be)\/(?:watch\?v=)?(\w{1,})(?:&?[^\?
 module.exports = async(message, args, client) => {
     const data = queues.get(message.guild.id);
     if (!data) {
-        message.member.voice.channel
-          ? message.member.voice.channel.join()
-              .then(conn => {
-                   queues.set(message.guild.id, new Queue(message, conn));
-                   client.emit('message', message);
-              })
-              .catch(err => message.channel.send(`おっと、エラーが発生したみたいですね\nエラー内容: ${err}`))
-          : (() => {
-              message.channel.send('ボイスチャンネルに参加してください');
-              const func = function (_, newState) {
-                  if (
-                      newState.channel &&
-                      newState.member.id === message.member.id
-                  ) newState.channel.join()
-                      .then(conn => {
-                          queues.set(message.guild.id, new Queue(message, conn));
-                          client.emit('message', message);
-                      })
-                      .catch(err => message.channel.send(`おっと、エラーが発生したみたいですね\nエラー内容: ${err}`))
-              }
-              client.on('voiceStateUpdate',func)
-              setTimeout(() => client.off('voiceStateUpdate',func), 10000);
-          })();
+        if (message.member.voice.channel) {
+            message.member.voice.channel.join()
+            .then(conn => {
+                queues.set(message.guild.id, new Queue(message, conn));
+                client.emit('message', message);
+            })
+            .catch(err => message.channel.send(`おっと、エラーが発生したみたいですね\nエラー内容: ${err}`))
+        } else {
+            message.channel.send('ボイスチャンネルに参加してください');
+            const func = function (_, newState) {
+                if (
+                    newState.channel &&
+                    newState.member.id === message.member.id
+                ) newState.channel.join()
+                .then(conn => {
+                    queues.set(message.guild.id, new Queue(message, conn));
+                    client.emit('message', message);
+                })
+                .catch(err => message.channel.send(`おっと、エラーが発生したみたいですね\nエラー内容: ${err}`))
+            }
+            client.on('voiceStateUpdate',func)
+            setTimeout(() => client.off('voiceStateUpdate',func), 10000);
+        }
     } else {
         const { connection, textChannel, voiceChannel } = data;
         const matched = args[0].match(regex);
@@ -41,7 +41,7 @@ module.exports = async(message, args, client) => {
             }));
             const filtered = result.items.filter(({duration}) => 
                duration.split(':').length <= 2 &&
-               6 >+ duration.split(':')[0]
+               +duration.split(':')[0] < 31
             );
             if (!result || !filtered.length)
             return void await message.reply(":x: No result...");
@@ -55,13 +55,14 @@ module.exports = async(message, args, client) => {
             });
             await message.reply(":white_check_mark: **Added:** " + song.title);
         } else {
-           let playlist = await ytpl(args[0]), addCount = 0;
-           for (let v of playlist.items) {
-              await serverQueue.addMusic(v.url, message).then(() => addCount++, _=>0);
-              await delay(1500);
-           }
-           if (1 < addCount) await message.channel.send(`:white_check_mark: Added ${addCount} songs!`);
-           else await message.channel.send(`:white_check_mark: Added ${addCount ? "1" : "no"} song!`);
+            let playlist = await ytpl(args[0]), addCount = 0;
+            for (let v of playlist.items) {
+                await serverQueue.addMusic(v.url, message).then(() => addCount++, _=>0);
+                await delay(1500);
+            }
+            await message.channel.send(
+                ":white_check_mark: Added " + (1 < addCount ? `${addCount} songs!` : `${addCount ? 1 : "no"} song!`)
+            );
         }
     }
 }
