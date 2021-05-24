715f1852cc91f5fc772f971023f13119eef5d08d
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-24 21:58:10 +0900

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,7 +1,6 @@
-let skipReqCount = 0;
-
 module.exports = async message => {
   const queue = queues.get(message.guild.id);
+  const skipReqs = queue.skipReqs;
   if (!queue)
     return void await message.reply(":x: There is no queue.");
   const now = queue.playingSong;
@@ -15,15 +14,16 @@ module.exports = async message => {
   if (!now.member.user.bot && queue.voiceChannel.members.has(now.member.id)) {
     if (now.member.id !== message.member.id) {
       const limen = Math.ceil(queue.voiceChannel.members.size / 2);
-      if (++skipReqCount >= limen) {
-        skipReqCount = 0;
+      skipReqs.add(message.member.id);
+      if (skipReqs.size >= limen) {
+        skipReqs.clear();
         try {
           queue.dispatcher.emit('finish');
           await message.reply(":fast_forward: Skipped!");
         } catch { }
         return;
       }
-      return void await message.reply("Skip request (${skipReqCount}/${limen})");
+      return void await message.reply(`Skip request (${skipReqs.size}/${limen})`);
     }
   }
   try {
