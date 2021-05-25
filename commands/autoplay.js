eb5de59c60f67c536dc33110b8428afd397dd1d6
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-25 18:48:13 +0900

    Create autoplay.js

---
+++ commands/autoplay.js
@@ -0,0 +1,6 @@
+module.exports = async message => {
+    const queue = queues.get(message.guild.id);
+    if (!queue)
+        return void await message.reply(Noqueue);
+    await message.reply(Messages.AutoPlay + ((queue.autoplay = !queue.autoplay) ? "ON" : "OFF"));
+}
