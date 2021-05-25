34cd08812bd6b425a523bffcc479e85f02afa443
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-25 19:00:54 +0900

    Update and rename commands.json to commands.js

---
+++ commands.js
@@ -0,0 +1,48 @@
+module.exports = {
+  aliases: {
+    h: 'help',
+    j: 'join',
+    p: 'play',
+    dc: 'disconnect',
+    q: 'queue',
+    lq: 'loopqueue',
+    qloop: 'loopqueue',
+    s: 'skip',
+    np: 'nowplay',
+    rm: 'remove',
+    find: 'search',
+    auto: 'autoplay'
+  },
+  commands: {
+    ping: { description: 'ping pong', args: [ [] ], details: '' },
+    nowplay: {
+      description: '今再生してる曲を表示するよ！',
+      args: [ [] ],
+      details: '引数は不要です(ง ˙-˙ )ง'
+    },
+    todo: {
+      description: 'ToDo List',
+      args: [ [ 'subcommand' ], [ 'subcommand', 'data' ] ],
+      details: ''
+    },
+    join: {
+      description: 'VCに参加します！',
+      args: [ [] ],
+      details: '引数は不要です(ง ˙-˙ )ง'
+    },
+    help: {
+      description: '使い方わからん！そんなあなたのために！',
+      args: [ [], [ 'コマンド' ] ],
+      details: '第一引数はなくてもコマンドのリストが出てくるよ！'
+    },
+    play: { args: [ [ 'query' ] ] },
+    loopqueue: { args: [ [] ] },
+    disconnect: { args: [ [] ] },
+    queue: { args: [ [], [ 'page' ] ] },
+    skip: { args: [ [], [ 'query' ] ] },
+    remove: { variadic: true, args: [ [ 'キュー内でのナンバー', '〃...' ] ] },
+    search: { args: [ [ '検索語句' ], [ '〃', 'フラグ' ] ] },
+    loop: { args: [ [] ] },
+    autoplay: { args: [ [] ] }
+  }
+}
