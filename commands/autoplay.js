7dcd39cce845d6e6e99a9328bb8ecb300b3f75f3
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-25 21:38:29 +0900

    Update autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -2,5 +2,6 @@ module.exports = async message => {
     const queue = queues.get(message.guild.id);
     if (!queue)
         return void await message.reply(Messages.Noqueue);
-    await message.reply(Messages.AutoPlay + ((queue.autoplay = !queue.autoplay) ? "ON" : "OFF"));
+    const enabled = queue.autoplay = !queue.autoplay;
+    await message.reply(Messages.AutoPlay + (enabled ? "ON" : "OFF"));
 }
