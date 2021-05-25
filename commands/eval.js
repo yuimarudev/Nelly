e8c180fad259f702c98c9a03c868c9fe778e52fb
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-25 21:53:34 +0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,6 @@
 module.exports = async (message, [code], client) => {
     if (!client.application.owner.members.has(message.user.id)) return;
+    let result;
     try {
       result = eval(code);
     } catch (e) {
@@ -7,6 +8,6 @@ module.exports = async (message, [code], client) => {
     }
     if (Object.prototype.toString.call(result) === "[object Error]")
     result = Error.prototype.toString.call(result);
-    else result = "```js\n" + inspect(result) + "```";
+    else result = "```js\n" + require('util').inspect(result) + "```";
     await message.channel.send(result);
 }
