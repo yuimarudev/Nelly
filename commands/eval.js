fefed6dc09faedaa4b5221123c368413e5f2ec40
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-25 21:46:24 +0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,14 +1,11 @@
-const toStr = Object.prototype.toString.call;
-
 module.exports = async (message, [code], client) => {
-    const owner = await client.fetchApplication();
-    if (!owner.members.has(message.user.id)) return;
+    if (!client.application.owner.members.has(message.user.id)) return;
     try {
       result = eval(code);
     } catch (e) {
       result = e;
     }
-    if (toStr(result) === "[object Error]")
+    if (Object.prototype.toString.call(result) === "[object Error]")
     result = Error.prototype.toString.call(result);
     else result = "```js\n" + inspect(result) + "```";
     await message.channel.send(result);
