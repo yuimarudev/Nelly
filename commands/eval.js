b96c4d31ec6c7b413a6603d024176365c793d401
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-26 12:32:28 +0900

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,25 +1,23 @@
-const vm = require('vm');
-let that = this;
+const { NodeVM } = require('vm2');
+
 module.exports = async (message, code, client) => {
     if (!(await client.application.fetch()).owner.members.has(message.author.id)) return;
     let result;
     try {
-      let Script = new vm.Script(code);
-      result = Script.runInThisContext({ timeout: 10000 });
+        const vm = new VM({
+            sandbox: {
+                ...global,
+                message,
+                client
+            },
+            require: true
+        });
+        result = Script.runInThisContext({ timeout: 10000 });
     } catch (e) {
-      result = e;
+        result = e;
     }
-    // while (result !== null && typeof result === "object" && Symbol.toStringTag in result) delete result[Symbol.toStringTag];
     if (Object.prototype.toString.call(result) === "[object Error]")
     result = Error.prototype.toString.call(result);
     else result = "```js\n" + require('util').inspect(result) + "```";
     await message.channel.send(result);
 }
-
-function Context(...args){
-  return vm.createContext({
-    ...global,
-    ...that,
-    ...args
-  });
-};
