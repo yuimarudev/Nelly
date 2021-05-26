dd2bb7f3ca3cb25862d11c5a88b8e4fbf88304dd
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-26 13:03:32 +0900

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,9 +1,10 @@
 const Queue = require('../structure/Queue.js');
 
 module.exports = async message => {
-    message.member.voice?.channel.join()
-      .then(conn => queues.set(message.guild.id, new Queue(message, conn)))
-      .catch(err => message.channel.send(`${Messages.AccidentMessage}\nエラー内容: ${err}`))
-    ? void 0
-    : message.channel.send(Messages.PleaseJoinVoiceChannelMessage)
+    if (!message.member.voice.channel) {
+        return void await message.reply(Messages. PleaseJoinVoiceChannelMessage);
+    }
+    message.member.voice.channel.join()
+    .then(conn => queues.set(message.guild.id, new Queue(message, conn)))
+    .catch(err => message.channel.send(`${Messages.AccidentMessage}\nエラー内容: ${err}`));
 }
