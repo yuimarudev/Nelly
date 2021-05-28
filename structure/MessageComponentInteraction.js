a162eaa0c4b8f6c60930e152845ae303d773b809
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-28 14:36:35 +0900

    Update MessageComponentInteraction.js

--- structure/MessageComponentInteraction.js
+++ structure/MessageComponentInteraction.js
@@ -1,15 +1,15 @@
 module.exports = class extends Discord.Interaction {
   constructor(client, data) {
     super(client, data);
-    this.deferred = false;
-    this.replied = false;
-    this.webhook = new Discord.WebhookClient(this.applicationID, this.token, this.client.options);
-    
     this.message = this.channel.messages.add(data.message);
     this.customID = data.data.custom_id;
     this.componentType = data.data.component_type;
-    this._data = data;
     Object.defineProperty(this, "isMessageComponent", {value: true});
+
+    this._data = data;
+    this.deferred = false;
+    this.replied = false;
+    this.webhook = new Discord.WebhookClient(this.applicationID, this.token, this.client.options);
   }
 }
 
