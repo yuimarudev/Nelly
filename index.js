1837994b809864c8936982c117813e5c276c9a6f
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-28 19:35:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const prefix = '%';
 ["MessageEmbed", "MessageAttachment"]
   .forEach(v => global[v] = Discord[v]);
 
+require('./ModifyAPIMessage.js');
 let dotenvPath = path.join(__dirname, '.env');
 if (fs.existsSync(dotenvPath)) {
   const env = dotenv.parse(fs.readFileSync(dotenvPath));
