19ba3f10d3ffcc641a33ee10505022c3df328d62
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-28 11:38:50 +0900

    Update extend.js

--- extensionStructure/extend.js
+++ extensionStructure/extend.js
@@ -26,72 +26,57 @@ class MessageComponentInteraction extends Discord.Interaction {
     Object.defineProperty(this, "isMessageComponent", {value: true});
   }
 
-  async defer(ephemeral) {
-    if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
-    await this.client.api.interactions(this.discordID, this.token).callback.post({
+  async defer({ ephemeral } = {}) {
+    if (this.deferred || this.replied) throw new Error('INTERACTION_ALREADY_REPLIED');
+    await this.client.api.interactions(this.id, this.token).callback.post({
       data: {
-        type: 6,
+        type: InteractionResponseTypes.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          flags: ephemeral ? 1 << 6 : null,
+          flags: ephemeral ? MessageFlags.FLAGS.EPHEMERAL : undefined,
         },
       },
     });
     this.deferred = true;
   }
 
-  async think(ephemeral) {
-    if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
-    await this.client.api.interactions(this.discordID, this.token).callback.post({
+  async reply(content, options) {
+    if (this.deferred || this.replied) throw new Error('INTERACTION_ALREADY_REPLIED');
+    const apiMessage = content instanceof APIMessage ? content : APIMessage.create(this, content, options);
+    const { data, files } = await apiMessage.resolveData().resolveFiles();
+
+    await this.client.api.interactions(this.id, this.token).callback.post({
       data: {
-        type: 5,
-        data: {
-          flags: ephemeral ? 1 << 6 : null,
-        },
+        type: InteractionResponseTypes.CHANNEL_MESSAGE_WITH_SOURCE,
+        data,
       },
+      files,
     });
-    this.deferred = true;
+    this.replied = true;
   }
 
-  get reply() {
-    let _send = async (content, options = {}) => {
-      if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
-      if (typeof (options) === 'boolean' && options === true) {
-        options = { flags: 1 << 6 }
-      }
+  async fetchReply() {
+    const raw = await this.webhook.fetchMessage('@original');
+    return this.channel?.messages.add(raw) ?? raw;
+  }
 
-      let { data: info, files } = await (content instanceof sendAPICallback ? content : sendAPICallback.create(this.client.channels.resolve(this.channel.id), content, options || {})).resolveData();
-      await this.client.api.interactions(this.discordID, this.token).callback
-      .post({
-         data: {
-           data: info,
-           type: options.type ? ([4, 5, 6, 7].includes(parseInt(options.type)) ? parseInt(options.type) : 4) : 4,
-         },
-         files
-      });
-      this.replied = true;
-    }
+  async editReply(content, options) {
+    const raw = await this.webhook.editMessage('@original', content, options);
+    return this.channel?.messages.add(raw) ?? raw;
+  }
 
-    let _fetch = async () => {
-      const raw = await this.webhook.fetchMessage('@original');
-      return this.channel ? this.channel.messages.add(raw) : raw;
-    }
+  async deleteReply() {
+    await this.webhook.deleteMessage('@original');
+  }
 
-    let _edit = async (content, options = {}) => {
-      if (this.deferred === false && this.replied === false) throw new Error('BUTTON_ALREADY_REPLIED: This button has no reply');
-      const raw = await this.webhook.editMessage('@original', content, options);
-      return this.channel ? this.channel.messages.add(raw) : raw;
-    }
+  async followUp(content, options) {
+    const apiMessage = content instanceof APIMessage ? content : APIMessage.create(this, content, options);
+    const { data, files } = await apiMessage.resolveData().resolveFiles();
 
-    let _delete = async () => {
-      if (this.deferred === false && this.replied === false) throw new Error('BUTTON_ALREADY_REPLIED: This button has no reply');
-      return await this.webhook.deleteMessage('@original');
-    }
+    const raw = await this.client.api.webhooks(this.applicationID, this.token).post({
+      data,
+      files,
+    });
 
-    return {
-      send: _send,
-      fetch: _fetch,
-      edit: _edit,
-      delete: _delete
-    }
+    return this.channel?.messages.add(raw) ?? raw;
   }
 }
