ae5337f1862d1e8b1953383d1d0c5f4777699826
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-28 10:01:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ class MessageComponentInteraction extends Discord.Interaction {
     super(client, data);
     this.deferred = false;
     this.replied = false;
-    this.webhook = new WebhookClient(this.applicationID, this.token, this.client.options);
+    this.webhook = new Discord.WebhookClient(this.applicationID, this.token, this.client.options);
     this.message = this.channel.messages.add(data.message);
     this.customID = data.data.custom_id;
     this.componentType = data.data.component_type;
