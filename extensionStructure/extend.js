be5267e0d79c1dca790365c91f70dd55fa6f1a93
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-28 11:27:24 +0900

    Update extend.js

--- extensionStructure/extend.js
+++ extensionStructure/extend.js
@@ -26,76 +26,72 @@ class MessageComponentInteraction extends Discord.Interaction {
     Object.defineProperty(this, "isMessageComponent", {value: true});
   }
 
-    async defer(ephemeral) {
-        if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
-        await this.client.api.interactions(this.discordID, this.token).callback.post({
-            data: {
-                type: 6,
-                data: {
-                    flags: ephemeral ? 1 << 6 : null,
-                },
-            },
-        });
-        this.deferred = true;
+  async defer(ephemeral) {
+    if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
+    await this.client.api.interactions(this.discordID, this.token).callback.post({
+      data: {
+        type: 6,
+        data: {
+          flags: ephemeral ? 1 << 6 : null,
+        },
+      },
+    });
+    this.deferred = true;
+  }
+
+  async think(ephemeral) {
+    if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
+    await this.client.api.interactions(this.discordID, this.token).callback.post({
+      data: {
+        type: 5,
+        data: {
+          flags: ephemeral ? 1 << 6 : null,
+        },
+      },
+    });
+    this.deferred = true;
+  }
+
+  get reply() {
+    let _send = async (content, options = {}) => {
+      if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
+      if (typeof (options) === 'boolean' && options === true) {
+        options = { flags: 1 << 6 }
+      }
+
+      let { data: info, files } = await (content instanceof sendAPICallback ? content : sendAPICallback.create(this.client.channels.resolve(this.channel.id), content, options || {})).resolveData();
+      await this.client.api.interactions(this.discordID, this.token).callback
+      .post({
+         data: {
+           data: info,
+           type: options.type ? ([4, 5, 6, 7].includes(parseInt(options.type)) ? parseInt(options.type) : 4) : 4,
+         },
+         files
+      });
+      this.replied = true;
     }
 
-    async think(ephemeral) {
-        if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
-        await this.client.api.interactions(this.discordID, this.token).callback.post({
-            data: {
-                type: 5,
-                data: {
-                    flags: ephemeral ? 1 << 6 : null,
-                },
-            },
-        });
-        this.deferred = true;
+    let _fetch = async () => {
+      const raw = await this.webhook.fetchMessage('@original');
+      return this.channel ? this.channel.messages.add(raw) : raw;
     }
 
-    get reply() {
-
-        let _send = async (content, options = {}) => {
-
-            if (this.deferred || this.replied) throw new Error('BUTTON_ALREADY_REPLIED: This button already has a reply');
-
-            if (typeof (options) === 'boolean' && options === true) {
-                options = { flags: 1 << 6 }
-            }
-
-            let { data: info, files } = await (content instanceof sendAPICallback ? content : sendAPICallback.create(this.client.channels.resolve(this.channel.id), content, options || {})).resolveData();
-
-            await this.client.api.interactions(this.discordID, this.token).callback
-                .post({
-                    data: {
-                        data: info,
-                        type: options.type ? ([4, 5, 6, 7].includes(parseInt(options.type)) ? parseInt(options.type) : 4) : 4,
-                    },
-                    files
-                });
-            this.replied = true;
-        }
-
-        let _fetch = async () => {
-            const raw = await this.webhook.fetchMessage('@original');
-            return this.channel ? this.channel.messages.add(raw) : raw;
-        }
-
-        let _edit = async (content, options = {}) => {
-            if (this.deferred === false && this.replied === false) throw new Error('BUTTON_ALREADY_REPLIED: This button has no reply');
-            const raw = await this.webhook.editMessage('@original', content, options);
-            return this.channel ? this.channel.messages.add(raw) : raw;
-        }
-
-        let _delete = async () => {
-            if (this.deferred === false && this.replied === false) throw new Error('BUTTON_ALREADY_REPLIED: This button has no reply');
-            return await this.webhook.deleteMessage('@original');
-        }
-
-        return {
-            send: _send,
-            fetch: _fetch,
-            edit: _edit,
-            delete: _delete
-        }
+    let _edit = async (content, options = {}) => {
+      if (this.deferred === false && this.replied === false) throw new Error('BUTTON_ALREADY_REPLIED: This button has no reply');
+      const raw = await this.webhook.editMessage('@original', content, options);
+      return this.channel ? this.channel.messages.add(raw) : raw;
     }
+
+    let _delete = async () => {
+      if (this.deferred === false && this.replied === false) throw new Error('BUTTON_ALREADY_REPLIED: This button has no reply');
+      return await this.webhook.deleteMessage('@original');
+    }
+
+    return {
+      send: _send,
+      fetch: _fetch,
+      edit: _edit,
+      delete: _delete
+    }
+  }
 }
