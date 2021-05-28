c0bb28b37e9aa999352532a0b16dd4f279a0a082
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-05-29 07:51:23 +0900

    Create ButtonClickCollector.js

---
+++ structure/ButtonClickCollector.js
@@ -0,0 +1,80 @@
+const { Collector, Collection } = Discord;
+const { Events } = Discord.Constants;
+
+module.exports = class ButtonClickCollector extends Collector {
+  constructor(message, filter, options) {
+    super(message.client, filter, options);
+    this.message = message;
+    this.users = new Collection();
+    this.total = 0;
+    this.empty = this.empty.bind(this);
+    this._handleChannelDeletion = this._handleChannelDeletion.bind(this);
+    this._handleGuildDeletion = this._handleGuildDeletion.bind(this);
+    this._handleMessageDeletion = this._handleMessageDeletion.bind(this);
+
+    const handler
+    this.client.incrementMaxListeners();
+    this.client.on('interaction', this.#_buttonInteractionHandler);
+    this.client.on(Events.MESSAGE_DELETE, this._handleMessageDeletion);
+    this.client.on(Events.CHANNEL_DELETE, this._handleChannelDeletion);
+    this.client.on(Events.GUILD_DELETE, this._handleGuildDeletion);
+
+    this.once('end', () => {
+      this.client.removeListener('interaction', this.#_buttonInteractionHandler);
+      this.client.removeListener(Events.MESSAGE_DELETE, this._handleMessageDeletion);
+      this.client.removeListener(Events.CHANNEL_DELETE, this._handleChannelDeletion);
+      this.client.removeListener(Events.GUILD_DELETE, this._handleGuildDeletion);
+      this.client.decrementMaxListeners();
+    });
+  }
+
+  #_buttonInteractionHandler(itr) {
+    if (itr.isMessageComponent) this.handleCollect(itr.button, itr.user);
+  }
+
+  collect(button, user) {
+    if (button.message.id !== this.message.id) return null;
+    if (this.filter(button, user)) {
+      this.emit('create', button, user);
+    }
+    return ButtonClickCollector.key(button);
+  }
+
+  dispose() { return null; }
+
+  empty() {
+    this.total = 0;
+    this.collected.clear();
+    this.users.clear();
+    this.checkEnd();
+  }
+
+  get endReason() {
+    if (this.options.max && this.total >= this.options.max) return 'limit';
+    if (this.options.maxEmojis && this.collected.size >= this.options.maxEmojis) return 'emojiLimit';
+    if (this.options.maxUsers && this.users.size >= this.options.maxUsers) return 'userLimit';
+    return null;
+  }
+
+  _handleMessageDeletion(message) {
+    if (message.id === this.message.id) {
+      this.stop('messageDelete');
+    }
+  }
+
+  _handleChannelDeletion(channel) {
+    if (channel.id === this.message.channel.id) {
+      this.stop('channelDelete');
+    }
+  }
+
+  _handleGuildDeletion(guild) {
+    if (this.message.guild && guild.id === this.message.guild.id) {
+      this.stop('guildDelete');
+    }
+  }
+
+  static key(button) {
+    return button.id;
+  }
+}
