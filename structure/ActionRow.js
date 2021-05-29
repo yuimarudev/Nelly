0710729fc7733a4aaa28c64d7d9f6c629f3adc35
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-30 07:36:04 +0900

    Update ActionRow.js

--- structure/ActionRow.js
+++ structure/ActionRow.js
@@ -1,10 +1,11 @@
 module.exports = class ActionRow {
-  buttons = [];
   addButtons(...buttons) {
+    if(!this.buttons) this.buttons = [];
     this.buttons.push(...buttons);
     return this;
   }
   spliceButtons(index, deleteCount, ...buttons) {
+    if(!this.buttons) this.buttons = [];
     this.buttons.splice(index, deleteCount, ...buttons);
     return this;
   }
