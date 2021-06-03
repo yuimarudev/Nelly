7cf6f6f2713d4b57cbdca8930370aec78f051fa6
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 20:22:10 +0900

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,27 +3,6 @@ import path from 'path';
 import dotenv from 'dotenv';
 import { on } from 'events';
 import leven from 'levenshtein';
-import discord from 'discord.js';
-
-const _export = {};
-_export.queues = new Discord.Collection();
-["MessageEmbed", "MessageAttachment"]
-  .forEach(v => _export[v] = discord[v]);
-_export.Discord = discord;
-_export.Messages = JSON.parse(fs.readFileSync('./lang/ja_jp.json'));
-_export.stringFormat = (...r) =>
-r.reduce((a, c, i) => a.replace(
-  new RegExp(`\\{${i}\\}`, "g"), c
-), r.shift());
-export const {
-  MessageEmbed,
-  MessageAttachment,
-  Discord,
-  Messages,
-  stringFormat,
-  queues
-} = _export;
-
 import SpaceSplit from './spliter.mjs';
 import commandArgs from './commands.js';
 import MessageComponentInteraction from './structure/MessageComponentInteraction.mjs';
