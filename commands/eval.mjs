e646821e545135189cc97bc15fe85941b6bfeb6e
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 16:12:24 +0900

    Update eval.mjs

--- commands/eval.mjs
+++ commands/eval.mjs
@@ -1,36 +1,36 @@
 import { VM } from 'vm2';
 
-export default (message, code, client) => {
-    if (!(await client.application.fetch()).owner.members.has(message.author.id)) return;
-    let result;
-    try {
-        const sandbox = { };
-        for (const key in global)
-        if ("global" !== key)
-        sandbox[key] = global[key];
-        Object.assign(sandbox, {
-            message,
-            client,
-            require,
-            process
-        });
-        const vm = new VM({
-            sandbox,
-            require: true,
-            timeout: 1000
-        });
-        result = await withTimeout(vm.run(code));
-    } catch (e) {
-        result = e;
-    }
-    if (result === void 0) return;
-    if (Object.prototype.toString.call(result) === "[object Error]")
+export default ((message, code, client) => {
+  if (!(await client.application.fetch()).owner.members.has(message.author.id)) return;
+  let result;
+  try {
+    const sandbox = { };
+    for (const key in global)
+    if ("global" !== key)
+      sandbox[key] = global[key];
+    Object.assign(sandbox, {
+      message,
+      client,
+      require,
+      process
+    });
+    const vm = new VM({
+      sandbox,
+      require: true,
+      timeout: 1000
+    });
+    result = await withTimeout(vm.run(code));
+  } catch (e) {
+    result = e;
+  }
+  if (result === void 0) return;
+  if (Object.prototype.toString.call(result) === "[object Error]")
     await message.channel.send(Error.prototype.toString.call(result));
-    else await message.channel.send(
-        (await import('util')).inspect(result),
-        { split: true, code: "js" }
-    );
-}
+  else await message.channel.send(
+    (await import('util')).inspect(result),
+    { split: true, code: "js" }
+  );
+})
 
 function withTimeout(promise) {
   const timeout = 10000;
