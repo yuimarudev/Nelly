bccd64d383f0e9beb5b50f79ada5604d6e6fcfbc
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 18:55:13 +0900

    Update join.mjs

--- commands/join.mjs
+++ commands/join.mjs
@@ -1,3 +1,12 @@
+import {
+  MessageEmbed,
+  MessageAttachment,
+  Discord,
+  Messages,
+  stringFormat,
+  queues
+} from '../index.mjs';
+
 import Queue from '../structure/Queue.mjs';
 
  export default (message => {
