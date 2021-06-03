d7cb4703517fa070ef3d3fe5805c1364b417f1a7
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 16:06:41 +0900

    Update and rename autoplay.js to autoplay.mjs

--- commands/autoplay.mjs
+++ commands/autoplay.mjs
@@ -1,7 +1,7 @@
-export default message => {
+export default (message => {
   const queue = queues.get(message.guild.id);
   if (!queue)
     return void await message.reply(Messages.NoQueue);
   const enabled = queue.autoplay = !queue.autoplay;
   await message.reply(Messages.AutoPlay + (enabled ? "ON" : "OFF"));
-}
+})
