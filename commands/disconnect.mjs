cd1e678c63b7bed415411e7b9e1151319c5c339f
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 16:08:35 +0900

    Update and rename disconnect.js to disconnect.mjs

--- commands/disconnect.mjs
+++ commands/disconnect.mjs
@@ -1,8 +1,8 @@
-export (message => {
+export default (message => {
   const queue = queues.get(message.guild.id);
   if (!queue) return;
   const cnct = queue.connection;
   if (!cnct) return;
   cnct.disconnect();
   message.react('👋').catch(console.log);
-});
+})
