49523403898a15430d3fe77974106ab97d8aea37
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 16:07:30 +0900

    Update disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -1,8 +1,8 @@
-module.exports = message => {
+export (message => {
   const queue = queues.get(message.guild.id);
   if (!queue) return;
   const cnct = queue.connection;
   if (!cnct) return;
   cnct.disconnect();
   message.react('👋').catch(console.log);
-};
+});
