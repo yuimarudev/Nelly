ef3ff688eb820f88d65082f53e9a466b386325bb
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 18:54:12 +0900

    Update autoplay.mjs

--- commands/autoplay.mjs
+++ commands/autoplay.mjs
@@ -1,3 +1,12 @@
+import {
+  MessageEmbed,
+  MessageAttachment,
+  Discord,
+  Messages,
+  stringFormat,
+  queues
+} from '../index.mjs';
+
 export default (message => {
   const queue = queues.get(message.guild.id);
   if (!queue)
