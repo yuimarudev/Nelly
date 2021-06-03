20a0b312f766cbadf820e3b61504cd792754fd05
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-03 18:56:07 +0900

    Update ping.mjs

--- commands/ping.mjs
+++ commands/ping.mjs
@@ -1,3 +1,12 @@
+import {
+  MessageEmbed,
+  MessageAttachment,
+  Discord,
+  Messages,
+  stringFormat,
+  queues
+} from '../index.mjs';
+
 export default ((message, _, client) => {
   return void await message.channel.send(`Pong!🏓 ${client.ws.ping}ms`);
 });
