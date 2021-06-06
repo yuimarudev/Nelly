9901831422d056929c3ac83b3a7d80c78fe86752
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-06-07 01:16:02 +0900

    Update execute.mjs

--- util/execute.mjs
+++ util/execute.mjs
@@ -1,29 +1,35 @@
 import { VM } from 'vm2';
 import { loopWhile } from 'deasync';
 
-function exe(code, sandbox) {
-  const ans = new VM({
-    sandbox: {
-      result: new VM({
-        sandbox,
-        timeout: 1000 // first timeout(sync)
-      }).run(code),
-      loopWhile
-    },
-    timeout: 3000 // second timeout(async)
-  }).run(`
-    (() => {
+const exe = (code, sandbox) => {
+	const result = new VM({
+		sandbox,
+		timeout: 1000
+	}).run(code);
+	return new VM({
+		sandbox: { result, loopWhile },
+		timeout: 8000
+	}).run(`(_ => {
       let v, d, r;
-      Promise.resolve(result).then(
+      if (typeof result.then !== "function") return result;
+      result.then(
         a => { v = a; d = true; }, 
         e => { v = e; r = d = true; }
       );
-      loopWhile(_=>!d);
+      loopWhile(_ => !d);
       const [value, rejected] = [v, r];
       if (rejected) throw value; return value;
-    })();
-  `);
-  return ans;
-}
+    })()`);
+};
 
-export default exe;
+export default function(...a) {
+  return new Promise((r, e) => {
+    process.nextTick(() => {
+      try {
+        r(exe(...a));
+      } catch(ex) {
+        e(ex);
+      }
+    });
+  })
+}
