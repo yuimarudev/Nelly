00b4509ef21304f465e750120e4d9ba43a2fd54c
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-06 17:59:36 +0900

    Create eval.mjs

--- commands/eval.mjs
+++ commands/eval.mjs
@@ -9,6 +9,10 @@ import {
 
 import { VM } from 'vm2';
 import { loopWhile } from 'deasync';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default async function(message, code, client) {
   if (!(await client.application.fetch()).owner.members.has(message.author.id)) return;
@@ -28,7 +32,7 @@ export default async function(message, code, client) {
       stringFormat,
       queues,
       process,
-      moduleImport: name => import(name)
+      require
     });
     const vm = new VM({
       sandbox,
@@ -66,3 +70,25 @@ function withTimeout(promise) {
   `);
 }
 
+function require() {
+  async function _require(path) {
+    path = path.resolve(__dirname, path);
+    const res = await import(path);
+    return res.default || res;
+  };
+  const _path = arguments[0];
+  let result = _require(_path);
+  let done = false;
+  let _return;
+  result
+  .then(v => {
+    _return = v;
+    done = true;
+  })
+  .catch(e => {
+    _return = e;
+    done = true;
+  });
+  loopWhile(() => !done);
+  return _return;
+};
