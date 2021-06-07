e8a8090d067529e9070a21dfb0d7a6087e860c91
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-06-08 02:54:25 +0900

    Update exeWorker.mjs

--- utils/exeWorker.mjs
+++ utils/exeWorker.mjs
@@ -1,4 +1,54 @@
 import { worker } from 'workerpool';
 import { VM } from 'vm2';
-const run = (code, sandbox) => new VM({ sandbox }).run(code);
+import {
+  MessageEmbed,
+  MessageAttachment,
+  Discord,
+  Messages,
+  stringFormat,
+  queues
+} from '../global.mjs';
+
+const client = new Discord.Client({
+  intents: Discord.Intents.NON_PRIVILEGED,
+  ws: { intents: Discord.Intents.NON_PRIVILEGED }
+});
+
+const run = (code, sandbox) => {
+  try {
+    const ch = client.channels.cache.get(sandbox.message.channel.id);
+    sandbox.message = ch.messages.add(sandbox.message);
+  } catch { }
+  Object.assign(sandbox, {
+    client,
+    MessageEmbed,
+    MessageAttachment,
+    Discord,
+    Messages,
+    stringFormat,
+    queues,
+    process
+  });
+  return new VM({ sandbox }).run(code);
+};
 worker({ run });
+
+function require(_path) {
+  let mod, done, exception;
+  import(_path).then(
+    value => {
+      mod = value;
+      done = true;
+    },
+    ex => {
+      exception = ex;
+      done = true;
+    }
+  );
+  loopWhile(() => !done);
+  if (exception) throw exception;
+  if (mod !== void 0 && mod !== null) {
+    return mod.default || mod;
+  }
+};
+require.prototype.toString(() => "function require() { }");
