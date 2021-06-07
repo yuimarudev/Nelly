63083db014434d50cbc055e098338f8ff87a518b
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-06-08 03:48:35 +0900

    Update global.mjs

--- global.mjs
+++ global.mjs
@@ -1,11 +1,20 @@
-import discord from 'discord.js';
+import Discord from 'discord.js';
 import fs from 'fs';
 
 const _export = {};
-_export.queues = new discord.Collection();
+_export.queues = new Discord.Collection();
 ["MessageEmbed", "MessageAttachment"]
-  .forEach(v => _export[v] = discord[v]);
-_export.Discord = discord;
+  .forEach(v => _export[v] = Discord[v]);
+_export.client = new Discord.Client({
+  intents: Discord.Intents.NON_PRIVILEGED,
+  ws: {
+    intents: Discord.Intents.NON_PRIVILEGED,
+    properties: {
+        $browser: 'God of Nelly'
+    }
+  }
+});
+_export.Discord = Discord;
 _export.Messages = JSON.parse(fs.readFileSync('./lang/ja_jp.json'));
 _export.stringFormat = (...r) =>
 r.reduce((a, c, i) => a.replace(
