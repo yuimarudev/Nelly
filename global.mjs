135d76dddaacd270496607bd9816e6b2093324c5
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-06-08 03:53:53 +0900

    Update global.mjs

--- global.mjs
+++ global.mjs
@@ -1,12 +1,10 @@
 import Discord from 'discord.js';
 import fs from 'fs';
-export Discord;
 
-const _export = {};
-_export.queues = new Discord.Collection();
+const queues = new Discord.Collection();
 ["MessageEmbed", "MessageAttachment"]
   .forEach(v => _export[v] = Discord[v]);
-_export.client = new Discord.Client({
+const client = new Discord.Client({
   intents: Discord.Intents.NON_PRIVILEGED,
   ws: {
     intents: Discord.Intents.NON_PRIVILEGED,
@@ -15,16 +13,16 @@ _export.client = new Discord.Client({
     }
   }
 });
-_export.Discord = Discord;
-_export.Messages = JSON.parse(fs.readFileSync('./lang/ja_jp.json'));
-_export.stringFormat = (...r) =>
+const Messages = JSON.parse(fs.readFileSync('./lang/ja_jp.json'));
+const stringFormat = (...r) =>
 r.reduce((a, c, i) => a.replace(
   new RegExp(`\\{${i}\\}`, "g"), c
 ), r.shift());
-export const {
+export {
   MessageEmbed,
   MessageAttachment,
   Messages,
   stringFormat,
-  queues
-} = _export;
+  queues,
+  Discord
+};
