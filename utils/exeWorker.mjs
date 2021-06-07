6bcf4413a22e2eaaea157aa9a1c6f93c84bda07a
csjscpp <79582228+csjscpp@users.noreply.github.com>
2021-06-08 03:45:55 +0900

    Update exeWorker.mjs

--- utils/exeWorker.mjs
+++ utils/exeWorker.mjs
@@ -6,40 +6,28 @@ import {
   Discord,
   Messages,
   stringFormat,
-  queues
+  queues,
+  client
 } from '../global.mjs';
 
-const client = new Discord.Client({
-  intents: Discord.Intents.NON_PRIVILEGED,
-  ws: { intents: Discord.Intents.NON_PRIVILEGED }
-});
-client.login(process.env.token);
-
-const run = (code, sandbox, clientData) => new Promise((s, e) => {
-  client.on("ready", () => {
-    try {
-      const ch = client.channels.cache.get(sandbox.message.channel.id);
-      sandbox.message = ch.messages.add(sandbox.message);
-    } catch { }
-    for (const u of clientData.users) client.users.add(u);
-    Object.assign(sandbox, {
-      client,
-      MessageEmbed,
-      MessageAttachment,
-      Discord,
-      Messages,
-      stringFormat,
-      queues,
-      process,
-      require
-    });
-    try {
-      s(new VM({ sandbox }).run(code));
-    } catch(ex) {
-      e(ex);
-    }
+const run = (code, sandbox) => {
+  Object.assign(sandbox, {
+    client,
+    MessageEmbed,
+    MessageAttachment,
+    Discord,
+    Messages,
+    stringFormat,
+    queues,
+    process,
+    require
   });
-});
+  try {
+    s(new VM({ sandbox }).run(code));
+  } catch(ex) {
+    e(ex);
+  }
+};
 worker({ run });
 
 function require(_path) {
