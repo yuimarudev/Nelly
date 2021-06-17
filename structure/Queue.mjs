de5c9d7fa3622281d91fbdccbc9aceb929a50b1f
IdioticDog <83163937+IdioticDog@users.noreply.github.com>
2021-06-17 18:44:31 +0900

    Update Queue.mjs

--- structure/Queue.mjs
+++ structure/Queue.mjs
@@ -49,7 +49,7 @@ async function play(queue) {
   queue.isPlaying = true;
   queue.skipReqs.clear();
   const song = queue.playingSong = queue.songs.shift();
-  const stream = ytdl(song.url)
+  const stream = ytdl(song.url,{quality:'highestaudio',filter:'audioonly'})
   .once('error', err => {
     queue.textChannel.send(
       new MessageEmbed()
